cc81935297f82edc3f6d7f7abff059b974c994ef
Eddie Ho <eddie.ho@and.digital>
2020-11-12 11:23:59 +0000

    Updated with repo details

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from aws_cdk import core
 
 from pipelines_webinar.pipeline_stack import PipelineStack
 
-PIPELINE_ACCOUNT = '123456789012'
+PIPELINE_ACCOUNT = '323040907683'
 
 app = core.App()
 PipelineStack(app, 'PipelineStack', env={
   'account': PIPELINE_ACCOUNT,
-  'region': 'eu-central-1',
+  'region': 'eu-west-2',
 })
 
 app.synth()
--- pipelines_webinar/pipeline_stack.py
+++ pipelines_webinar/pipeline_stack.py
@@ -5,7 +5,7 @@ from aws_cdk import pipelines
 
 from .webservice_stage import WebServiceStage
 
-APP_ACCOUNT = '123456789012'
+APP_ACCOUNT = '323040907683'
 
 class PipelineStack(core.Stack):
   def __init__(self, scope: core.Construct, id: str, **kwargs):
@@ -22,8 +22,8 @@ class PipelineStack(core.Stack):
         action_name='GitHub',
         output=source_artifact,
         oauth_token=core.SecretValue.secrets_manager('github-token'),
-        owner='OWNER**REPLACEME',
-        repo='REPO**REPLACEME',
+        owner='eddie-ho',
+        repo='awscdk-pipeline',
         trigger=cpactions.GitHubTrigger.POLL),
 
       synth_action=pipelines.SimpleSynthAction(
@@ -35,7 +35,7 @@ class PipelineStack(core.Stack):
 
     pre_prod_app = WebServiceStage(self, 'Pre-Prod', env={
       'account': APP_ACCOUNT,
-      'region': 'eu-central-1',
+      'region': 'eu-west-2',
     })
     pre_prod_stage = pipeline.add_application_stage(pre_prod_app)
     pre_prod_stage.add_actions(pipelines.ShellScriptAction(
@@ -52,7 +52,7 @@ class PipelineStack(core.Stack):
 
     pipeline.add_application_stage(WebServiceStage(self, 'Prod', env={
       'account': APP_ACCOUNT,
-      'region': 'eu-central-1',
+      'region': 'eu-west-2',
     }))
 
 
