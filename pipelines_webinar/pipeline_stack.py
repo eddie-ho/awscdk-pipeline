e2393d64ce41a605a9b33fd563a52ec1570d4e93
Eddie Ho <eddie.ho@and.digital>
2020-11-12 12:03:06 +0000

    added branch to main

--- pipelines_webinar/pipeline_stack.py
+++ pipelines_webinar/pipeline_stack.py
@@ -24,6 +24,7 @@ class PipelineStack(core.Stack):
         oauth_token=core.SecretValue.secrets_manager('github-token'),
         owner='eddie-ho',
         repo='awscdk-pipeline',
+        branch='main', 
         trigger=cpactions.GitHubTrigger.POLL),
 
       synth_action=pipelines.SimpleSynthAction(
